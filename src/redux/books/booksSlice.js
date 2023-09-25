ac27aac1af0159144aaaa5bdab75e202432f7100
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-09-25 13:05:35 +0430

    add reducers and actions

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    books: [],
+};
+
+const bookSlice = createSlice({
+    name: 'book',
+    initialState,
+    reducers:{
+        addBook:(state,action) => {
+            // add new book to array
+            state.books.push(action,payload);
+        },
+        removeBook:(state,action) => {
+            // remove book by its ID
+            return state.filter(book => book.id !== action.payload)    
+        }
+    }
+})
+
+export const {addBook, removeBook} = bookSlice.actions;
+export default bookSlice.reducer;
