ea2211581c8857ff7ad36504ac7841a76c81e7e0
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-09-25 21:21:00 +0430

    connect redux with react components

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,34 +1,12 @@
-import React, { useState } from 'react';
 import BookForm from './BookForm';
 import BookList from './BookList';
 
-const Books = () => {
-  // Define state to manage books
-  const [books, setBooks] = useState([]);
-
-  // Function to handle book creation
-  const handleAddBook = (newBook) => {
-    // Add the new book to the list of books
-    setBooks([...books, newBook]);
-  };
-
-  // Function to handle book deletion
-  const handleDeleteBook = (bookTitle) => {
-    // Remove the book at the specified index
-    const updatedBooks = books.filter((book) => book.title !== bookTitle);
-    setBooks(updatedBooks);
-  };
-
-  return (
-    <div>
-      <h1>Books</h1>
-      {/* Book Form */}
-      <BookForm onAddBook={handleAddBook} />
-
-      {/* Book List */}
-      <BookList books={books} onDelete={handleDeleteBook} />
-    </div>
-  );
-};
+const Books = () => (
+  <div>
+    <h1>Books</h1>
+    <BookForm />
+    <BookList />
+  </div>
+);
 
 export default Books;
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,20 +1,39 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  books: [],
+  books: [
+    {
+      item_id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
 };
 
 const bookSlice = createSlice({
   name: 'book',
-  initialState,
+  initialState, // Updated initial state
   reducers: {
     addBook: (state, action) => {
       // add new book to array
       state.books.push(action.payload);
     },
     removeBook: (state, action) => {
-    // remove book by its ID
-      state.filter((book) => book.id !== action.payload);
+      // remove book by its ID
+      state.books = state.books.filter((book) => book.item_id !== action.payload);
     },
   },
 });
